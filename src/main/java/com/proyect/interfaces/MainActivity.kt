d8d0249dca037eae1f4e35af5a2035dcf92e244a
Diego Puebla <43054925+Wosser147@users.noreply.github.com>
2019-10-26 11:04:33 -0500

    V 0.5

---
+++ src/main/java/com/proyect/interfaces/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.proyect.interfaces
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+
+    fun passActivity(view: View) {
+        // Handler code here.
+        val intent = Intent(applicationContext, UsuariosFamiliares::class.java)
+        startActivity(intent);
+    }
+}
